db1dbc3457610ef8b8600e5181093b6468a25003
Alena Bulanova <kxzws54@gmail.com>
2021-10-18 20:25:59 +0300

    feat: add st-stack.js

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -12,19 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+module.exports = class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
+    this.storage[this.count] = element;
+    this.count++;
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
+    if (this.count === 0) return undefined;
+    let element = this.storage[this.count - 1];
+    this.count--;
+    delete this.storage[this.count];
+    return element;
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
+    return this.storage[this.count - 1];
     // remove line with error and write your code here
   }
-}
+};
