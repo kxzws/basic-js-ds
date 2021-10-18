af0db3a9094a7cb4237bcfe58a73ba8815ed3237
Alena Bulanova <kxzws54@gmail.com>
2021-10-18 21:19:31 +0300

    feat: add st-queue.js

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
+    const createList = (elem) => {
+      if (elem === this.tail) return {
+        value: this.tail.value,
+        next: null
+      };
+      return {
+        value: elem.value,
+        next: createList(elem.next)
+      };
+    }
+    return createList(this.head);
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
+    if (!this.head) {
+      this.head = new ListNode(value);
+      this.tail = this.head;
+    } else {
+      let newNode = new ListNode(value);
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+      this.tail = newNode;
+    }
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
+    if (!this.head) return undefined;
+    let element = this.head;
+    this.head = this.head.next;
+    let result = element.value;
+    return result;
     // remove line with error and write your code here
   }
-
-}
+};
